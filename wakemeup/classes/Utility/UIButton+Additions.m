5d56a3e49c1538be20a129bfd4946a307c23ad51
Chandan Shetty SP <chandanshetty01@gmail.com>
2015-04-23 23:44:30 +0800

    localisation changes

--- wakemeup/classes/Utility/UIButton+Additions.m
+++ wakemeup/classes/Utility/UIButton+Additions.m
@@ -12,20 +12,19 @@
 
 -(void)bottomAlignText
 {
+    self.titleLabel.minimumScaleFactor = 0.5;
     // the space between the image and text
     CGFloat spacing = 6.0;
     
     // lower the text and push it left so it appears centered
     //  below the image
     CGSize imageSize = self.imageView.image.size;
-    self.titleEdgeInsets = UIEdgeInsetsMake(
-                                              0.0, - imageSize.width, - (imageSize.height + spacing), 0.0);
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (imageSize.height + spacing), 0.0);
     
     // raise the image and push it right so it appears centered
     //  above the text
     CGSize titleSize = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName: self.titleLabel.font}];
-    self.imageEdgeInsets = UIEdgeInsetsMake(
-                                              - (titleSize.height + spacing), 0.0, 0.0, - titleSize.width);
+    self.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + spacing), 0.0, 0.0, - titleSize.width);
 }
 
 @end
